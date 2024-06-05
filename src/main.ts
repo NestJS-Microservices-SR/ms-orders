dc3f65b59e311f7d802fd1428b2d2a4eb40d044c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-06-05 11:55:53 -0400

    create controller order service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main-Orders');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  await app.listen();
+  logger.log(`Running on port ${envs.port}`);
 }
 bootstrap();
