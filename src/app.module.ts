3c37d15feb17d42860fc6d8286722a65bcb2dd65
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-06-30 18:13:51 -0400

    Remove AppService and update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [OrdersModule],
-  providers: [AppService],
 })
 export class AppModule {}
